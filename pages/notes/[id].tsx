de885634ce0427eaa0a44bc016833fe9bd4373b5
Kevin-Shallvari-Tot <kshallvari@gmail.com>
2022-08-15 18:59:06 +0200

    using route params

--- pages/notes/[id].tsx
+++ pages/notes/[id].tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const NoteWithId = () => {
+
+    const { query: { id } } = useRouter()
+
     return (
-        <div>Note page with id </div>
+        <div>Note page with id= {id} </div>
     )
 }
 
